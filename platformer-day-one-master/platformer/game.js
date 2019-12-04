ca5d554242ed739c46aa120063e0dc66f21b68f3
Fabrice-maker <56657118+Fabrice-maker@users.noreply.github.com>
2019-12-04 18:02:57 -0500

    Add files via upload

---
+++ platformer-day-one-master/platformer/game.js
@@ -0,0 +1,48 @@
+let hero = new Hero()
+let world = [
+	new Ground(2, 2, 18, 2),
+	new Ground(8, 5, 2, 1),
+	new Ground(12, 7, 6, 1),
+	new Ground(22, 6, 4, 1),
+]
+
+let keyPressed = {}
+window.addEventListener('keydown', event => {
+  keyPressed[event.code] = true
+})
+window.addEventListener('keyup', event => {
+  keyPressed[event.code] = false
+})
+
+function loop() {
+  if(keyPressed['Space']) {
+    hero.jump()
+  }
+  if(keyPressed['ArrowLeft']) {
+    hero.moveLeft()
+  }
+  if(keyPressed['ArrowRight']) {
+  hero.moveRight()
+  }
+
+  // change state
+ hero.step()
+  // draw all
+  erase()
+  world.forEach(g => g.draw())
+  hero.draw()
+
+  setTimeout(() => loop(), 1000 / 60)
+}
+
+// wait for images to load
+async function loadGame() {
+  await heroStandSprite.loaded
+  await heroJumpSprite.loaded
+  await heroWalkSprite1.loaded
+  await heroWalkSprite2.loaded
+  await groundSprite.loaded
+  await groundTopSprite.loaded
+  loop()
+}
+loadGame()
